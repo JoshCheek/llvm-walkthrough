07a2fe3f3f988afd40b194fa8e3bd5ab5ecf991f
Josh Cheek <josh.cheek@gmail.com>
2016-05-30 00:17:34 -0600

    Lexes extern

--- lib/kaleidoscope/lex.rb
+++ lib/kaleidoscope/lex.rb
@@ -9,20 +9,48 @@ module Kaleidoscope
     end
 
     def call
-      return to_enum :call unless block_given?
-      yield get_token until stream.eof?
+      return to_enum(:call) unless block_given?
+      yield get_token(stream) until stream.eof?
     end
 
     private
 
     attr_accessor :stream
 
-    def get_token
-      token = ""
-      token << stream.getc
-      token << stream.getc
-      token << stream.getc
-      [:def, token]
+    def get_token(stream)
+      remove_whitespace(stream)
+      chars = next_nonwhitespace(stream)
+      if chars == 'def'
+        [:def, chars]
+      elsif chars == 'extern'
+        [:extern, chars]
+      else
+        raise "Handle this: #{chars.inspect}"
+      end
     end
+
+    def remove_whitespace(stream)
+      until stream.eof?
+        char = stream.getc
+        next if char =~ /\s/
+        stream.ungetc char
+        break
+      end
+    end
+
+    def next_nonwhitespace(stream)
+      chars = ""
+      until stream.eof?
+        char = stream.getc
+        if char =~ /\s/
+          stream.ungetc char
+          break
+        else
+          chars << char
+        end
+      end
+      chars
+    end
+
   end
 end
